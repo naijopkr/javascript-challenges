af2f8e677ea003cfb7878ecebdf72123bd4fd65d
Ariel Barcellos <barcellos.ariel@gmail.com>
2018-08-22 12:36:01 -0500

    Object diff resolved

--- object-diff/object-diff.js
+++ object-diff/object-diff.js
@@ -1,5 +1,37 @@
 const diff = (newCode, oldCode) => {
-  return []
+  const result = []
+
+  const getFlatObj = (path, obj) => {
+    path += path === '' ? '' : '.'
+    let flatObj = {}
+    Object.keys(obj).forEach(key => {
+      if (typeof obj[key] === 'object') {
+        flatObj = { ...flatObj, ...getFlatObj(path + key, obj[key]) }
+      } else {
+        flatObj[path + key] = obj[key]
+      }
+    })
+    return flatObj
+  }
+
+  oldCode = getFlatObj('', oldCode)
+  newCode = getFlatObj('', newCode)
+
+  //Removed
+  Object.keys(oldCode).forEach(oldKey => {
+    if(!newCode[oldKey]) {
+      result.push(['-', oldKey, oldCode[oldKey]])
+    }
+  })
+
+  //Added
+  Object.keys(newCode).forEach(newKey => {
+    if(!oldCode[newKey]) {
+      result.push(['+', newKey, newCode[newKey]])
+    }
+  })
+
+  return result
 }
 
 module.exports = diff

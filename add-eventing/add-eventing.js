c18ecd6400a999e446f009d6c5286a902c5623de
Ariel Barcellos <barcellos.ariel@gmail.com>
2018-08-30 17:39:38 -0500

    Completed add-eventing challenge

--- add-eventing/add-eventing.js
+++ add-eventing/add-eventing.js
@@ -1,4 +1,25 @@
-const addEventing = function (obj) {
+const addEventing = (obj) => {
+  let events = {}
+  return (
+    {
+      value: obj,
+      on: (name, callback) => {
+        if (callback && typeof name === 'string') {
+          if (!events[name]) {
+            events[name] = []
+          }
+          events[name].push(callback)
+        }
+      },
+      trigger: (name, ...args) => {
+        if (events[name]) {
+          events[name].forEach(callback => {
+            callback(...args)
+          })
+        }
+      }
+    }
+  )
 }
 
 module.exports = addEventing

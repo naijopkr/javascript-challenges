f1f6be4f07f5cfc79fcd3a5885602270f1e36f82
Ariel Barcellos <barcellos.ariel@gmail.com>
2018-08-31 17:07:39 -0500

    Completed Babylonian method challenge

--- babylonian-method/babylonian.js
+++ babylonian-method/babylonian.js
@@ -1,5 +1,19 @@
 const squareRoot = (radicand) => {
-  return radicand
+  if (isNaN(radicand) || radicand < 0) {
+    return NaN
+  }
+  const precision = 0.000000000000001
+  let lastGuess = 0
+  let guess = radicand/4
+  while (radicand/guess !== guess && Math.abs(radicand/guess - guess) >= precision) {
+    if (radicand/guess > guess) {
+      lastGuess = guess
+      guess = (radicand + guess)/2
+    } else {
+      guess = guess - (guess - lastGuess)/2
+    }
+  }
+  return radicand/guess
 }
 
 module.exports = squareRoot

3c075a442d406d323fde9cc8eeb553ce9edd2815
Ariel Barcellos <barcellos.ariel@gmail.com>
2018-08-31 22:03:01 -0500

    Solved number-frequency

--- number-frequency/most-recurrent.js
+++ number-frequency/most-recurrent.js
@@ -1,5 +1,22 @@
 const mostRecurrent = (set) => {
-  return set
+  let moreRecurrent = null
+  let numbers = {}
+  set.forEach(number => {
+    if (numbers[number]) {
+      numbers[number]++
+    } else {
+      numbers[number] = 1
+    }
+  })
+  for (number in numbers) {
+    if (!moreRecurrent) {
+      moreRecurrent = number
+    } else if (numbers[number] > numbers[moreRecurrent]) {
+      moreRecurrent = number
+    }
+  }
+
+  return Number(moreRecurrent)
 }
 
 module.exports = mostRecurrent

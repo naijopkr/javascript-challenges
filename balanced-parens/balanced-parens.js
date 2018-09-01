f85fff65189135427dc33a7bd54af0d0906bb4db
Ariel Barcellos <barcellos.ariel@gmail.com>
2018-08-31 19:01:26 -0500

    Solved balanced-parens

--- balanced-parens/balanced-parens.js
+++ balanced-parens/balanced-parens.js
@@ -1,5 +1,22 @@
 const parensAreBalanced = (input) => {
-  return false
+  const charMap = {
+    40: 41,
+    91: 93,
+    123: 125,
+    closeChars: [41, 93, 125]
+  }
+  let lookingFor = []
+
+  for (var i = 0; i < input.length; i++) {
+    if (charMap[input.charCodeAt(i)]) {
+      lookingFor.push(charMap[input.charCodeAt(i)])
+    } else if (lookingFor[lookingFor.length - 1] === input.charCodeAt(i)) {
+      lookingFor.pop()
+    } else if (charMap.closeChars.indexOf(input.charCodeAt(i)) !== -1) {
+      return false
+    }
+  }
+  return lookingFor.length === 0
 }
 
 module.exports = parensAreBalanced

b5295a5f19f0cbb3bcfcf9ba5806c8bcec3c7575
Ariel Barcellos <barcellos.ariel@gmail.com>
2018-09-04 16:30:06 -0500

    Solved stack-machine-calculator

--- stack-machine-calculator/stack-machine-calculator.js
+++ stack-machine-calculator/stack-machine-calculator.js
@@ -1,5 +1,31 @@
 const stackMachineCalculator = (instructions) => {
-  return instructions
+  var stack = instructions.split(' ')
+  var result = []
+  for (const el of stack) {
+    switch (el) {
+      case 'DUP':
+        result.push(result[result.length-1])
+        break
+      case 'POP':
+        result.pop()
+        break
+      case '+':
+        var first = result.pop()
+        var second = result.pop()
+        result.push(Number(first) + Number(second))
+        break
+      case '-':
+        var first = result.pop()
+        var second = result.pop()
+        result.push(Number(first) - Number(second))
+        break
+      default:
+        result.push(el)
+        break
+    }
+  }
+
+  return Number(result.pop())
 }
 
 module.exports = stackMachineCalculator

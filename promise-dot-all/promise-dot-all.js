9abcdf802bd405d8305693053626215d2ef951d3
Ariel Barcellos <barcellos.ariel@gmail.com>
2018-09-04 15:22:11 -0500

    Solved promise-dot-all

--- promise-dot-all/promise-dot-all.js
+++ promise-dot-all/promise-dot-all.js
@@ -1,5 +1,16 @@
-function all (promises) {
-  return {}
+const all = async (promises) => {
+  var output = []
+  for (const promise of promises) {
+    var el = await new Promise((resolve, reject) => {
+      try {
+        resolve(promise)
+      } catch (e) {
+        reject(promise)
+      }
+    })
+    output.push(el)
+  }
+  return output
 }
 
 module.exports = all
